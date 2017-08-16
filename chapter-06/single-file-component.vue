2a1ba3c75ad0afb680aa01d7765dab239e4123a4
Erik Hanchett <erik.hanchett@gmail.com>
2017-08-15 20:49:21 -0700

    addded chapter 6

---
+++ chapter-06/single-file-component.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
