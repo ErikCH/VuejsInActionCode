8dbe7794114f9173f885ae869023e835671e6401
Erik Hanchett <erik.hanchett@gmail.com>
2017-11-14 12:42:41 -0800

    Added chapter 9

---
+++ chapter-09/jsx-example/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <HelloWorld header="1" name="Erik"></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ chapter-09/jsx-example/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'HelloWorld',
+  render(h) {
+    return (
+      <div>
+        <div class="text-center"
+        on-click={this.pressed}
+        domPropsInnerHTML={this.msg}></div>
+      </div>
+    )
+  },
+  data () {
+    return {
+      welcome: 'Hello World ',
+      msg: `<h${this.header}>Hello World ${this.name}</h${this.header}>`,
+    }
+  },
+  methods: {
+    pressed() {
+      alert('Clicked')
+    }
+
+  },
+  props:['header', 'name']
+}
+</script>
+
+<style scoped>
+</style>
